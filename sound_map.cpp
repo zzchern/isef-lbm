386b5620594be5f55d6cf669ddbe2a753dfb345a
Sergey Dukanov <sergeydukanov@gmail.com>
2014-03-29 22:06:30 +0400

    GPL headers added.

--- sound_map.cpp
+++ sound_map.cpp
@@ -1,3 +1,23 @@
+/***
+    This file is part of 'isef-lbm'. Program for simulation of acoustic
+    waves using Lattice Boltzmann Method.
+    
+    Copyright (C) 2013  Arkadiy Prigojin, Sergey Dukanov
+
+    'isef-lbm' is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Foobar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ***/ 
+
 /*
   This idea is based on creationd "sound maps".
   Sound Map [SM] - type of displaing of information, which shows, what points of fieled
